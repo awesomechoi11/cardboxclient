f5dfff4720d2faae62bcfd551ef8f3bd14d914c9
brandon <brandonchoi2016@gmail.com>
2023-07-08 20:56:33 -0700

    search result pack modal

--- components/CardPack/CardDisplay/CardRow.js
+++ components/CardPack/CardDisplay/CardRow.js
@@ -1,8 +1,33 @@
-export default function CardRow() {
-    return <div className="card-row">row</div>;
+import ImageViewer from "@components/Modals/ImageViewer/ImageViewer";
+import { normalizeImageSrc } from "@components/general/NormalizedImage";
+import draftjsToHtml from "draftjs-to-html";
+
+export default function CardRow({ data }) {
+    const { term, definition } = data;
+    return (
+        <div className="relative flex w-full px-6 py-2 bg-blue-100 shadow-xl rounded-xl">
+            <div className="border-r border-[D3D3D3] w-1/2 flex-grow-0  box-border flex items-center flex-shrink-0 mr-3">
+                {term && <CardFace data={term} />}
+            </div>
+            <div className="flex flex-col items-center flex-grow gap-3">
+                {definition && <CardFace data={definition} />}
+            </div>
+        </div>
+    );
 }
 
-function Card() {
-    // const { cardSide, cardData } = useCardDisplayRowState();
-    return <div className="card"></div>;
+function CardFace({ data }) {
+    const { image, content } = data;
+    let imgSrc = normalizeImageSrc(image);
+    return (
+        <>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            {imgSrc && <ImageViewer imgData={data.image} />}
+            {/* {imgSrc && <img layout="fill" src={imgSrc} alt="card" />} */}
+            <div
+                className="content"
+                dangerouslySetInnerHTML={{ __html: draftjsToHtml(content) }}
+            />
+        </>
+    );
 }
--- components/Modals/ModalUtils.js
+++ components/Modals/ModalUtils.js
@@ -4,13 +4,22 @@ import { atomFamily, useRecoilState, useResetRecoilState } from "recoil";
 import FilePickerModal from "./FilePicker/_FilePickerModal";
 import LoginModal from "./Login/LoginModal";
 import PublishModal from "./PublishModal/_PublishModal";
+import { SearchResultPreviewModal } from "@components/Search/Search.ResultCard";
+import Button from "@components/general/Button";
+import { twMerge } from "tailwind-merge";
+import { ImageViewerModal } from "./ImageViewer/ImageViewer";
 
 export default function ModalRoot() {
     return (
-        <div id="modal-root"  className="absolute overflow-auto pointer-events-none w-full h-screen z-10 top-0 ">
+        <div
+            id="modal-root"
+            className="absolute top-0 z-10 w-full h-screen overflow-auto pointer-events-none "
+        >
+            <ImageViewerModal />
             <LoginModal />
             <FilePickerModal />
             <PublishModal />
+            <SearchResultPreviewModal />
         </div>
     );
 }
@@ -48,17 +57,23 @@ export function useModal(modalId) {
     };
 }
 
-export function ModalWrapper({ modalId, children, className, ...props }) {
+export function ModalWrapper({
+    modalId,
+    children,
+    className,
+    innerClassName,
+    ...props
+}) {
     const { toggle, isOpen } = useModal(modalId);
 
     return (
         <AnimatePresence>
             {isOpen && (
                 <motion.div
-                    className={clsx(
+                    className={twMerge(
                         "modal-container",
-                        className,
-                        "cursor-pointer w-full min-h-full p-10 flex items-center flex-col box-border bg-blue-300/50 opacity-0 "
+                        "cursor-pointer w-full min-h-full p-1 tablet:p-4 desktop:p-10 flex items-center flex-col box-border bg-blue-900/75 opacity-0 ",
+                        className
                         // isOpen && "active"
                     )}
                     {...props}
@@ -72,22 +87,21 @@ export function ModalWrapper({ modalId, children, className, ...props }) {
                     }}
                 >
                     <div
-                        className="content border-box cursor-auto relative bg-brown-50 rounded-xl w-[600px] h-[770px] py-11 px-10 m-auto text-center"
+                        className={twMerge(
+                            "content border-box cursor-auto relative bg-brown-50 rounded-xl w-[600px] py-11 px-10 m-auto text-center",
+                            innerClassName
+                        )}
                         onClick={(e) => {
                             e.stopPropagation();
                         }}
                     >
-                        <button
-                            className="flex justify-center items-center absolute -translate-y-11 translate-x-40 cursor-pointer bg-brown-50 h-6 w-6 rounded-xl hover:scale-110 transition linear duration-600"
+                        <Button
+                            size="roundedDefault"
+                            variant="secondary"
+                            className="absolute right-0 items-center justify-center hidden transition bg-blue-100 cursor-pointer translate-x-9 hover:scale-110 linear duration-600 desktop:block "
                             onClick={() => toggle()}
-
-                            // tabIndex="0"
                         >
                             <svg
-                                style={{
-                                    width: "24",
-                                    height: "24",
-                                }}
                                 width="24"
                                 height="24"
                                 viewBox="0 0 24 24"
@@ -95,25 +109,13 @@ export function ModalWrapper({ modalId, children, className, ...props }) {
                                 xmlns="http://www.w3.org/2000/svg"
                             >
                                 <path
-                                    fillRule="evenodd"
-                                    clipRule="evenodd"
+                                    fill-rule="evenodd"
+                                    clip-rule="evenodd"
                                     d="M17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289Z"
-                                    fill="#674433"
-                                />
-                                <path
-                                    fillRule="evenodd"
-                                    clipRule="evenodd"
-                                    d="M17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289Z"
-                                    fill="#674433"
-                                />
-                                <path
-                                    fillRule="evenodd"
-                                    clipRule="evenodd"
-                                    d="M17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289Z"
-                                    fill="#674433"
+                                    fill="#144B9F"
                                 />
                             </svg>
-                        </button>
+                        </Button>
                         {children}
                     </div>
                 </motion.div>
