b81589b91342483c7655865aae5d2e6f12a50d56
brandon <brandonchoi2016@gmail.com>
2023-07-14 12:59:50 -0700

    delete old card pack apge

--- pages/card-pack-editor/[cardPackId].js
+++ pages/card-pack-editor/[cardPackId].js
@@ -11,137 +11,142 @@ import Navbar from "../../components/Navbar";
 import PlaceholderColumn from "../../components/PlaceholderColumn";
 
 export default function CreatePack() {
-  const isMobile = useIsMobile();
-  return (
-    <>
-      <Head>
-        <title key="title">Card Pack Editor - Flippy - Flashcard App</title>
-      </Head>
-      <Navbar />
-      {isMobile ? (
-        <div className="placeholder-wrapper flex justify-center">
-          <PlaceholderColumn
-            options={{
-              imageKey: "catOnBook",
-              message: {
-                title: "Sorry, Not Ready Yet :(",
-                description: "Card Pack Editing is not avaiable in mobile yet!",
-              },
-            }}
-          />
-        </div>
-      ) : (
+    const isMobile = useIsMobile();
+    return (
         <>
-          (
-          <WaitForMongo>
-            <Main />
-          </WaitForMongo>
-          )
+            <Head>
+                <title key="title">
+                    Card Pack Editor - Flippy - Flashcard App
+                </title>
+            </Head>
+            <Navbar />
+            {isMobile ? (
+                <div className="flex justify-center placeholder-wrapper">
+                    <PlaceholderColumn
+                        options={{
+                            imageKey: "catOnBook",
+                            message: {
+                                title: "Sorry, Not Ready Yet :(",
+                                description:
+                                    "Card Pack Editing is not avaiable in mobile yet!",
+                            },
+                        }}
+                    />
+                </div>
+            ) : (
+                <>
+                    (
+                    <WaitForMongo>
+                        <Main />
+                    </WaitForMongo>
+                    )
+                </>
+            )}
         </>
-      )}
-    </>
-  );
+    );
 }
 
 function Main() {
-  const { query, isReady } = useRouter();
-  const { cardPackId } = query;
+    const { query, isReady } = useRouter();
+    const { cardPackId } = query;
 
-  const { isAnon } = useMongo();
-  const router = useRouter();
+    const { isAnon } = useMongo();
+    const router = useRouter();
 
-  return isAnon ? (
-    <div className="placeholder-wrapper flex justify-center">
-      <PlaceholderColumn
-        options={{
-          imageKey: "oopsCat",
-          message: {
-            title: "Not Allowed",
-            description: "You need an account to create or edit card packs!",
-          },
-          action: {
-            label: "Browse Packs Instead",
-            props: {
-              onClick: () => {
-                router.push("/browse");
-              },
-            },
-          },
-        }}
-      />
-    </div>
-  ) : (
-    <>
-      {!isReady && (
-        <div className="placeholder-wrapper flex justify-center">
-          <PlaceholderColumn presetKey="loading" />
+    return isAnon ? (
+        <div className="flex justify-center placeholder-wrapper">
+            <PlaceholderColumn
+                options={{
+                    imageKey: "oopsCat",
+                    message: {
+                        title: "Not Allowed",
+                        description:
+                            "You need an account to create or edit card packs!",
+                    },
+                    action: {
+                        label: "Browse Packs Instead",
+                        props: {
+                            onClick: () => {
+                                router.push("/browse");
+                            },
+                        },
+                    },
+                }}
+            />
         </div>
-      )}
-      {isReady && <Inner cardPackId={cardPackId} />}
-    </>
-  );
+    ) : (
+        <>
+            {!isReady && (
+                <div className="flex justify-center placeholder-wrapper">
+                    <PlaceholderColumn presetKey="loading" />
+                </div>
+            )}
+            {isReady && <Inner cardPackId={cardPackId} />}
+        </>
+    );
 }
 
 export const CardPackDataContext = createContext();
 CardPackDataContext.displayName = "CardPackDataContext";
 function Inner({ cardPackId }) {
-  const { user, db, isAnon } = useMongo();
-  const result = useQuery(
-    ["card-pack-editor", user.id, cardPackId, isAnon],
-    () =>
-      db.collection("cardpackDrafts").findOne({
-        _id: cardPackId,
-        author: user.id,
-      }),
-    {
-      // refetchOnWindowFocus: true,
-      enabled: !!db,
-    }
-  );
+    const { user, db, isAnon } = useMongo();
+    const result = useQuery(
+        ["card-pack-editor", user.id, cardPackId, isAnon],
+        () =>
+            db.collection("cardpackDrafts").findOne({
+                _id: cardPackId,
+                author: user.id,
+            }),
+        {
+            // refetchOnWindowFocus: true,
+            enabled: !!db,
+        }
+    );
 
-  return (
-    <main
-      id="CreatePack"
-      className="px-[540px] relative flex flex-col items-center gap-[48px] mb-[128px]"
-    >
-      {result.isLoading && <PlaceholderColumn presetKey="loading" />}
-      {result.isError && <PlaceholderColumn presetKey="error" />}
-      {result.isSuccess &&
-        (result.data ? (
-          <CardPackDataContext.Provider value={result}>
-            <div className="title-1 text-lg font-semibold text-blue-600 my-1">
-              Card Pack Editor
-            </div>
-            <CreatePackDetailsForm />
-            <div className="w-full my-6 mx-0 opacity-20 h-[2px] bg-blue-600" />
-            <CreatePackDisplay />
-          </CardPackDataContext.Provider>
-        ) : (
-          <PlaceholderColumn
-            options={{
-              imageKey: "oopsCat",
-              message: {
-                title: "Something went wrong...",
-                description: (
-                  <>
-                    Card Pack might be deleted. Otherwise, Try refreshing! If it
-                    keeps persisting please reach out to us on{" "}
-                    <Link
-                      href="https://discord.gg/QC3yHFySAV"
-                      target="_blank"
-                      className="text-blue-600 font-bold mx-2 my-0"
-                    >
-                      discord
-                    </Link>
-                    !
-                  </>
-                ),
-              },
-            }}
-          />
-        ))}
-    </main>
-  );
+    return (
+        <main
+            id="CreatePack"
+            className="px-[540px] relative flex flex-col items-center gap-[48px] mb-[128px]"
+        >
+            {result.isLoading && <PlaceholderColumn presetKey="loading" />}
+            {result.isError && <PlaceholderColumn presetKey="error" />}
+            {result.isSuccess &&
+                (result.data ? (
+                    <CardPackDataContext.Provider value={result}>
+                        <div className="my-1 text-lg font-semibold text-blue-600 title-1">
+                            Card Pack Editor
+                        </div>
+                        <CreatePackDetailsForm />
+                        <div className="w-full my-6 mx-0 opacity-20 h-[2px] bg-blue-600" />
+                        <CreatePackDisplay />
+                    </CardPackDataContext.Provider>
+                ) : (
+                    <PlaceholderColumn
+                        options={{
+                            imageKey: "oopsCat",
+                            message: {
+                                title: "Something went wrong...",
+                                description: (
+                                    <>
+                                        Card Pack might be deleted. Otherwise,
+                                        Try refreshing! If it keeps persisting
+                                        please reach out to us on{" "}
+                                        <Link
+                                            href="https://discord.gg/QC3yHFySAV"
+                                            target="_blank"
+                                            className="mx-2 my-0 font-bold text-blue-600"
+                                        >
+                                            discord
+                                        </Link>
+                                        !
+                                    </>
+                                ),
+                            },
+                        }}
+                    />
+                ))}
+        </main>
+    );
 }
 
 // .aggregate([
